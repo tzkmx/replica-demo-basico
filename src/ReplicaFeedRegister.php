8bbe6db3ac19ec5ce37ea169ce83343a7af58e05
Jesus E. Franco Martinez <tezcatl@fedoraproject.org>
2020-06-17 23:37:20 -0500

    Fix #1 documented and adding enhancements as todo annotations

--- src/ReplicaFeedRegister.php
+++ src/ReplicaFeedRegister.php
@@ -2,22 +2,29 @@
 
 class ReplicaFeedRegister
 {
-    /** @var string */
+    /** @var string description for value required to fetch posts for this replica archive */
     public $sourceUrlDescription;
+
     /**
-     * CategoryMeta constructor.
+     * Sets the description of URL field for create/edit forms, and hooks the methods
+     * for adding the custom fields as well as updating the meta value in replica taxonomy term.
+     * @uses __() function to make the description localizable.
      */
     public function __construct()
     {
         $this->sourceUrlDescription = __('URL of a WordPress site with REST API to pull posts from', 'replica_demo');
         add_action('init', [$this, 'registerReplicaTaxonomy'], 20);
-        add_action('replica_add_form_fields', [$this, 'replicaMeta']);
+        add_action('replica_add_form_fields', [$this, 'createReplicaMeta']);
         add_action('replica_edit_form_fields', [$this, 'editReplicaMeta'], 10);
         add_action('created_replica', [$this, 'saveReplicaMeta']);
         add_action('edited_replica', [$this, 'updateReplicaMeta'], 10);
     }
 
-    public function replicaMeta()
+    /**
+     * appends and input field to give this replica archive the url where to pull posts from.
+     * @uses _e() function to output label and making it localizable
+     */
+    public function createReplicaMeta()
     {
         ?>
         <div class="form-field term-group">
@@ -30,6 +37,11 @@ class ReplicaFeedRegister
         <?php
     }
 
+    /**
+     * Shows the current URL configured for Replica archive and allows updating it.
+     * @uses get_term_meta() to pull the value of the term already in database.
+     * @param $term WP_Term Taxonomy Replica Term being updated
+     */
     public function editReplicaMeta( $term ){
         $source_url = get_term_meta( $term->term_id, 'source_url', true );
         ?>
@@ -47,20 +59,40 @@ class ReplicaFeedRegister
         <?php
     }
 
+    /**
+     * Saves the source_url if not empty to allow pulling posts from that REST API endpoint.
+     * @param $term_id int id of Replica term archive being created.
+     * @uses esc_url() to validate that only clean_url are being saved.
+     * @uses add_term_meta() to save unique value for the Replica term.
+     * @todo validate that URL is actually a valid and available REST API posts endpoint
+     */
     public function saveReplicaMeta( $term_id ){
-        if( isset( $_POST['source_url'] ) && '' !== $_POST['source_url'] ){
-            $url = esc_url( $_POST['source_url'], ['http', 'https'] );
+        if( isset( $_POST['source_url'] ) && '' !== trim($_POST['source_url']) ){
+            $url = esc_url( trim($_POST['source_url']), ['http', 'https'] );
             add_term_meta( $term_id, 'source_url', $url, true );
         }
     }
 
+    /**
+     * Updates the meta field source_url if not empty and only valid schemas http/https.
+     * @param $term_id int id of Replica term archive being updated.
+     * @uses esc_url() to validate that only clean_url are being saved.
+     * @uses update_term_meta() to update single value rather than add a new URL.
+     * @todo validate that URL is actually a valid and available REST API posts endpoint
+     */
     public function updateReplicaMeta( $term_id ){
-        if( isset( $_POST['source_url'] ) && '' !== $_POST['source_url'] ){
-            $url = esc_url( $_POST['source_url'], ['http', 'https'] );
+        if( isset( $_POST['source_url'] ) && '' !== trim($_POST['source_url']) ){
+            $url = esc_url( trim($_POST['source_url']), ['http', 'https'] );
             update_term_meta( $term_id, 'source_url', $url );
         }
     }
 
+    /**
+     * Register the Replica taxonomy, to configure archives where to show 3rd party
+     * sites posts imported using REST API.
+     * @uses register_taxonomy() to configure public availability of archives and UI.
+     * @uses register_term_meta() to register source_url metadata and exposing it via REST API.
+     */
     public function registerReplicaTaxonomy()
     {
         $labels = [
